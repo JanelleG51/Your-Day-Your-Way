496e55cc0e864bdcb64801562e4f8586843e2c7c
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-07-25 20:36:01 +0000

    test: successfully link registration process to mongodb - user created

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    
+});
+
+$("#password").on("focusout", function (e) {
+    if ($(this).val() != $("#passwordConfirm").val()) {
+        $("#passwordConfirm").removeClass("valid").addClass("invalid");
+    } else {
+        $("#passwordConfirm").removeClass("invalid").addClass("valid");
+    }
+});
+
+$("#passwordConfirm").on("keyup", function (e) {
+    if ($("#password").val() != $(this).val()) {
+        $(this).removeClass("valid").addClass("invalid");
+    } else {
+        $(this).removeClass("invalid").addClass("valid");
+    }
+});
