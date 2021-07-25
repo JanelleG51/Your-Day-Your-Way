a1de2857ac09a6f8bee162443935622f29fa7957
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-07-25 19:07:01 +0000

    func: add registration function to app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,8 +19,27 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/landing")
 def landing_page():
-    return render_template("index.html")
+    return render_template("landing.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        exsiting_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
