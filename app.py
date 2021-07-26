f7463fb76af9a99aa269a71fd7ca95838d95df1a
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-07-26 13:41:22 +0000

    feat: add login functionality and username and password validation

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def landing_page():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-    
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -44,6 +43,29 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login.html"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
