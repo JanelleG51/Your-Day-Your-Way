61e4cd68c97ed1724a44577e166e34deb0219369
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2021-08-12 20:52:06 +0000

    bug test: reset automatic deploy to Heroku

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
     Flask, flash, render_template,
-    redirect, request, session, url_for)s
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from functools import wraps
